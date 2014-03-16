00745dee5a7566e410fac5168cd94d4438be9848
Anas Nakawa <anas.nakawa@gmail.com>
2014-03-16 15:19:50 +0400

    hotml skeletons

---
+++ index.js
@@ -0,0 +1,38 @@
+// -----------------------------------------------------------------
+// hotml - v0.0.1 
+// (c) Anas Nakawa - <anas.nakawa@gmail.com>
+// License: MIT (http://www.opensource.org/licenses/mit-license.php)
+// -----------------------------------------------------------------
+
+var fs = require( 'fs' );
+
+/**
+ * Parse given `hotml` string into html
+ *
+ * @param {string} input
+ * @param {function} callback
+ */
+
+function parseString( input, callback ) {
+
+}
+
+/**
+ * Parse given `hotml` file into html
+ *
+ * @param {string} path
+ * @param {function} callback
+ */
+
+function parseFile( path, callback ) {
+	fs.readFile( path, function( e, file ) {
+		if( e ) {
+			return callback( e, null );
+		}
+
+		parseString( content, callback );
+	});
+}
+
+module.exports.parseString = parseString;
+module.exports.parseFile = parseFile;
