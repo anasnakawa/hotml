cd1e7a447405a9fe8f53f07e56414c31d5ec1e5c
Anas Nakawa <anas.nakawa@gmail.com>
2014-03-19 16:48:36 +0400

    travis :sparkles:

--- gruntfile.js
+++ gruntfile.js
@@ -6,39 +6,34 @@
 
 module.exports = function( grunt ) {
 
-	grunt.initConfig({
+  grunt.initConfig({
 
-		nodeunit: {
-			tests: [ 'test/*_test.js' ]
-		}
+    simplemocha: {
+      options: {
+        //   globals: [ 'should' ]
+        // , timeout: 3000
+        // , ignoreLeaks: false
+        // , grep: '*-test'
+        // , ui: 'bdd'
+        // , reporter: 'tap'
+        }
+        , all: { 
+          src: [ 'test/*.js' ] 
+        }
+    }
 
-		, simplemocha: {
-			options: {
-				//   globals: [ 'should' ]
-				// , timeout: 3000
-				// , ignoreLeaks: false
-				// , grep: '*-test'
-				// , ui: 'bdd'
-				// , reporter: 'tap'
-		    }
-		    , all: { 
-		    	src: [ 'test/*.js' ] 
-		    }
-		}
+    , clean: {
+      tests: [ 'test/tmp' ]
+    }
 
-		, clean: {
-			tests: [ 'test/tmp' ]
-		}
+  });
 
-	});
+  // offical plugins
+  grunt.loadNpmTasks( 'grunt-contrib-clean' );
 
-	// offical plugins
-	grunt.loadNpmTasks( 'grunt-contrib-nodeunit' );
-	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+  // other
+  grunt.loadNpmTasks( 'grunt-simple-mocha' );
 
-	// other
-	grunt.loadNpmTasks( 'grunt-simple-mocha' );
-
-	grunt.registerTask( 'test', [ 'clean', 'nodeunit' ] );
-	grunt.registerTask( 'mocha', [ 'simplemocha' ] );
+  grunt.registerTask( 'mocha', [ 'simplemocha' ] );
+  grunt.registerTask( 'travis', [ 'clean', 'simplemocha' ] );
 }
