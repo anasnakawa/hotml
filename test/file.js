934f20e7bfa3ad758e8bbc8fc679d4f0230854d5
Anas Nakawa <anas.nakawa@gmail.com>
2014-03-23 18:36:54 +0400

    type checking update

--- test/file.js
+++ test/file.js
@@ -6,5 +6,7 @@ var expect = require( 'expect.js' );
 describe( 'file type detection', function() {
 	it( 'should detect valid file types for hotml', function() {
 		expect( file.isValidType( './hotml/alert.hotml' ) ).to.equal( true );
+		expect( file.isValidType( './hotml/alert.js' ) ).to.equal( true );
+		expect( file.isValidType( './hotml/alert.html' ) ).to.equal( true );
 	});
 });
